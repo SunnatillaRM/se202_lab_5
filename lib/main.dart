22ab8475ecc84ec34a0c2c30978a474ccc143957
SunnatillaRM <147365791+SunnatillaRM@users.noreply.github.com>
2025-09-26 22:55:16 +0500

    Smth

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,63 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp()); // removed const
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( // removed const
       home: Scaffold(
-        body: Center(
-          child: Container(
-            margin: const EdgeInsets.all(20.0),
-            padding: const EdgeInsets.all(16.0),
-            height: 150,
-            width: 100,
-            decoration: BoxDecoration(
-              color: Colors.redAccent,
-              borderRadius: BorderRadius.circular(12),
+        body: SafeArea(
+          child: Center(
+            child: 
+
+            // Task 1
+            /*
+            Container(
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(16.0),
+              height: 150,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              child: Icon(Icons.home, size: 36),
             ),
-            child: const Icon(Icons.home, size: 36),
+            */
+
+            // Task 2
+            /*
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("One, two, buckle my shoe"),
+                Text("Three, four, knock at the door"),
+                Text("Five, six, pick up sticks"),
+                Text("Seven, eight, lay them straight"),
+              ],
+            ),
+            */
+
+            // Task 3
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextField(
+                controller: TextEditingController(), // non-const
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Enter your password",
+                  icon: Icon(Icons.lock),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
           ),
         ),
       ),
     );
   }
 }
-// TODO Implement this library.
